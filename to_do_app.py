7da30e2745ba3daebc5ac5101ec29f1f6ecffc1e
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-06 17:38:21 +0200

    print_usage refactored

--- to_do_app.py
+++ to_do_app.py
@@ -21,12 +21,8 @@ class Controller():
             if self.list_argv[0] == "-l":
                 view.print_list()
 
-
-            #elif self.list_argv[0] == "a"
-            #
-            # if self.list_argv[0] == "-l":
-            #     pass
-
+            elif self.list_argv[0] == "-a":
+                view.add_new_task()
 
 class Model():
     pass
@@ -37,27 +33,25 @@ class View():
         pass
 
     def print_usage(self):
-        print("Python Todo application\n"
-            "=======================\n\n"
-
-            "Command line arguments:\n"
-             "-l   Lists all the tasks\n"
-             "-a   Adds a new task\n"
-             "-r   Removes an task\n"
-             "-c   Completes an task")
+        self.content = open("usage.txt", "r")
+        self.read_content = self.content.read()
+        print(self.read_content)
 
     def print_list(self):
-            self.content = open("database.txt", "r")
-            self.read_content = self.content.readlines()
-            if os.stat("database.txt").st_size == 0:
-                print("No todos today!")
-            else:
-                for i in range(0, len(self.read_content)):
-                    result = ""
-                    self.read_content[i].strip("True")
-                    result += str(i+1) + " - " + self.read_content[i].__str__()
-                    #return result
-                    print(result)
+        self.content = open("database.txt", "r")
+        self.read_content = self.content.readlines()
+        if os.stat("database.txt").st_size == 0:
+            print("No todos today!")
+        else:
+            for i in range(0, len(self.read_content)):
+                result = ""
+                self.read_content[i].strip("True")
+                result += str(i+1) + " - " + self.read_content[i].__str__()
+                print(result)
+
+    def add_new_task(self):
+        pass
+
 
 
 
