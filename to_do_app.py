5b3155d0c0e2907e223d941bbba437868769f8a7
Vera Nemedy-Varga <n.v.vera@gmail.com>
2017-04-06 16:35:30 +0200

    print_list works for -l command line argument

--- to_do_app.py
+++ to_do_app.py
@@ -1,27 +1,41 @@
 import sys
-#import database.txt
+#import database
+# content = open("database.txt", 'r')
+# read_content = content.readlines()
+# print(read_content)
 
 class Controller():
 
     def __init__(self):
-        pass
+        self.list_argv = []
+        self.arg_reader()
+        self.controller()
 
     def arg_reader(self):
-        self.list_argv = []
         if len(sys.argv) <= 1:
             self.list_argv = []
         else:
             self.list_argv = sys.argv[1:]
 
+    def controller(self):
         if len(self.list_argv) == 0:
             view.print_usage()
         else:
-            pass
-    	# if( arguments[0] == '-l' ):
-    	# 	print('Addolunk ilyet', arguments[1])
+            if self.list_argv[0] == "-l":
+                view.print_list()
+
+            #elif self.list_argv[0] == "a"
+            #
+            # if self.list_argv[0] == "-l":
+            #     pass
+
 
 class Model():
     pass
+    # def database_open(self):
+    #     self.content = open(database.txt, 'a+')
+    #     self.read_content = self.content.read()
+    #     print(self.read_content)
 
 
 class View():
@@ -38,8 +52,27 @@ class View():
              "-r   Removes an task\n"
              "-c   Completes an task")
 
-controller = Controller()
+    def print_list(self):
+            self.content = open("database.txt", "r")
+            self.read_content = self.content.readlines()
+            # self.todolist = []
+            # self.todolist.append(self.read_content)
+            #self.read_content.split('\n')
+            for i in range(0, len(self.read_content)):
+                result = ""
+                result += str(i+1) + ". " + self.read_content[i].__str__()
+                #return result
+                print(result)
+
+
+
 view = View()
+model = Model()
+controller = Controller()
 
 controller.arg_reader()
+#view.print_list()
+#controller.controller("l")
+#model.database_open()
+
 #view.print_usage()
